0be59831f355cb023f53d48377fa8640318f85e6
KT <thun.kristian@gmail.com>
2024-07-07 10:04:42 +0200

    server is upp

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+    console.log("Sever Listening on PORT:", PORT)
+})
+
+
+app.get("/status", (request, response) => {
+    const status = {
+        "Status": "Running"
+    };
+
+    response.send(status)
+
+});
