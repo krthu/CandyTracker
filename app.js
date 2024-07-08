77870913d8e934c1e4fc7eee9f48ffd708307107
KT <thun.kristian@gmail.com>
2024-07-08 10:06:30 +0200

    Starting to add api endpoints for candy

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Candy } = require('./db');
 const app = express();
 
 app.use(express.json())
@@ -12,11 +13,58 @@ app.listen(PORT, () => {
 })
 
 
-app.get("/status", (request, response) => {
-    const status = {
-        "Status": "Running"
-    };
+app.get("/candy", async (req, res) => {
+    try {
+        const candys = await Candy.findAll();
+        res.send({candys})
+    }
+    catch (error) {
+        res.status(500).send({ error: error.message })
+    }
+});
 
-    response.send(status)
+app.post("/candy", async (req, res) => {
+    const { name } = req.body;
+    console.log("Posted the name: " + name)
+    try {
+        const candy = await Candy.create({name})
+        res.status(201).send(candy)
+
+    }catch(error){
+        res.status(400).send({error: error.message})
+    }
+
+});
+
+app.get("/candy/:id", async (req, res) => {
+    const { id } = req.params
+    console.log(`Fetching candy with id:${id}`)
+    try { 
+        const candy = await Candy.findByPk(id);
+        if (candy) {
+            res.status(200).send(candy)
+        } else{
+            res.status(404).send({error: `Candy not found`});
+        }
+    } catch {
+        res.status(500).send({error: error.message})
+    }
+});
+
+app.delete('/candy/:id', async(req, res) => {
+    const { id } = req.params
+
+    try {
+        const candy = await Candy.findByPk(id);
+        if (candy) {
+            console.log("Hello!")
+            await candy.destroy();
+            res.status(200).send({message: `Candy ${candy.name} deleted successfully` });
+        } else{
+            res.status(404).send({error: "Candy not found"});
+        }
+    } catch{
+        res.status(500).send({error: error.message});
+    }
 
 });
